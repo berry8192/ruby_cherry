4a58ae2c7f6000a8602863d6c18d776c4217abcc
berry8192 <berryrt8192@gmail.com>
2021-06-16 22:58:57 +0900

    add7

--- test/fizz_buzz.rb
+++ test/fizz_buzz.rb
@@ -9,5 +9,6 @@ class Fizz_BuzzTest < Minitest::Test
         assert_equal '4', fizz_buzz(4)
         assert_equal 'buzz', fizz_buzz(5)
         assert_equal 'fizz', fizz_buzz(6)
+        assert_equal '7', fizz_buzz(7)
     end
 end
