87b5f94aeef0af573b12013bcb2600630fef441f
berry8192 <berryrt8192@gmail.com>
2021-06-22 18:30:14 +0900

    :sparkles: to_ints実装

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -3,3 +3,8 @@ def to_hex(r, g, b)
         hex += c.to_s(16).rjust(2, '0')
     end
 end
+
+def to_ints(hex)
+    r, g, b=hex[1..2], hex[3..4], hex[5..6]
+    [r, g, b].map(&:hex)
+end
--- test/rgb_test.rb
+++ test/rgb_test.rb
@@ -8,7 +8,9 @@ class Rgb_Test < Minitest::Test
         assert_equal '#4e828c', to_hex(78, 130, 140)
     end
 
-    # def test_to_int
-    #     assert_equal '1', fizz_buzz(1)
-    # end
+    def test_to_ints
+        assert_equal [0, 0, 0], to_ints("#000000")
+        assert_equal [255, 255, 255], to_ints("#ffffff")
+        assert_equal [49, 65, 89], to_ints("#314159")
+    end
 end
